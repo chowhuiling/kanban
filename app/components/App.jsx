66b4c697ea2777b61e98c93a4b8f1834f6e5474c
Chow Hui Ling <huiling.chow@gmail.com>
2016-01-03 17:56:43 +0800

    working note app + beautified

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -21,15 +21,44 @@ export default class App extends React.Component {
       }
       ]
     };
+
+    //bind the "this" context to the function so "this" works in the function
+    this.addNote = this.addNote.bind(this);
   }
 
   render() {
     const notes = this.state.notes;
     return (
         <div>
-          <Notes notes={notes} /> 
+          <button className="add-note" onClick={this.addNote}>+</button>
+          <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote} /> 
         </div>
       );
   }
+  //addNote = () => { //uncomment this line and remove the binding event above to use the new syntax.
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([{
+               id: uuid.v4(),
+              task: 'New task'
+             }])
+    });
+    console.log('add note');
+  }
+  editNote = (id, task) => {
+    const notes = this.state.notes.map((note) => {
+      if (note.id === id) {
+        note.task = task;
+      }
 
+      return note;
+    });
+    this.setState({notes});
+  }
+  deleteNote = (id) => {
+    console.log("deleting node:",id);
+    this.setState({
+      notes: this.state.notes.filter((note) => note.id !== id)
+    });
+  }
 }
