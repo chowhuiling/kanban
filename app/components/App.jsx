0d0df11a6491097ab37c784daa110a5970c06229
Chow Hui Ling <huiling.chow@gmail.com>
2016-01-04 00:07:55 +0800

    use alt-container to remove boilerplate code of binding to NoteStore

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,41 +1,27 @@
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = NoteStore.getState();
-
-    //bind the "this" context to the function so "this" works in the function
-    this.addNote = this.addNote.bind(this);
-  }
-
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
   
-  storeChanged = (state) => {
-    //must initialize the state so that it points to the right context.
-    this.setState(state);
-  }
-
   render() {
-    const notes = this.state.notes;
     return (
         <div>
           <button className="add-note" onClick={this.addNote}>+</button>
-          <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote} /> 
+          <AltContainer stores={[NoteStore]}
+                        inject={{
+                          notes: () => NoteStore.getState().notes
+                        }}
+                        >
+            <Notes onEdit={this.editNote} onDelete={this.deleteNote} /> 
+          </AltContainer>
         </div>
       );
   }
-  //addNote = () => { //uncomment this line and remove the binding event above to use the new syntax.
-  addNote() {
+  addNote = () => { //uncomment this line and remove the binding event above to use the new syntax.
+  // addNote() {
     NoteActions.create({task: 'New task'});
     
     console.log('add note');
