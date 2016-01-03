94e6a9dd403e11c034c5d92c071316f62d4071b3
Chow Hui Ling <huiling.chow@gmail.com>
2016-01-03 23:29:14 +0800

    correct typo errors and import required libs so that Note app works.

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,10 +1,11 @@
 import uuid from 'node-uuid';
+import assign from 'object-assign';
 import alt from '../libs/alt';
-import NodeActions from '../actions/NoteActions';
+import NoteActions from '../actions/NoteActions';
 
 class NoteStore {
   constructor() {
-    //use bindActions to map each action in NodeActions to a method by name.
+    //use bindActions to map each action in NoteActions to a method by name.
     this.bindActions(NoteActions);
     this.notes = [{id: uuid.v4(), task:'test'}];
   }
@@ -31,13 +32,13 @@ class NoteStore {
 
   delete(noteId) {
     this.setState({
-      notes: this.notes.filter((note) => note.id !== nodeId)
+      notes: this.notes.filter((note) => note.id !== noteId)
     });
   }
 }
 
 /* Connect the store with Alt using alt.createStore.
- * note that assigning the "NodeStore" label is not required, 
+ * note that assigning the "NoteStore" label is not required, 
  * but it protects code against minification and possible collisions.
  * the label is esp important when we persist the data.
  */
